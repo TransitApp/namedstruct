cd6bfd8f1c3b4fe0e428e9ff25dae3b8c954b1cf
Nicholas Paun <nicholas@transit.app>
2020-03-16 11:46:46 -0400

    Cannot have two things named bitfield2 apparently

--- tests.py
+++ tests.py
@@ -276,7 +276,7 @@ def generateTests():
 
     add(Struct("testStruct37")
         .addReference("stringRef", None, targetType=String().getType())
-        .addNullReference("bitFieldReference", BitField("bitField2").add("aField", 0, 13).getType())
+        .addNullReference("bitFieldReference", BitField("bitField2000").add("aField", 0, 13).getType())
         .addNullReference("int8ArrayRef", SimpleArray(n_types.INT8, [], 16).getType(), referenceBitWidth=16)
         .addReference("uint8Ref", None, referenceBitWidth=8, targetType=n_types.INT8))
 
