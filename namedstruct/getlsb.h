36988ccb4ffcf8b54acef2f5aba1dab0dba36b95
Rodrigo Hausen <hausen@compscinet.org>
2020-06-27 01:38:20 -0400

    use inline asm

--- namedstruct/getlsb.h
+++ namedstruct/getlsb.h
@@ -12,7 +12,7 @@
 namespace namedstruct {
     /* Documentation **********************************************************************/
 
-    /** extracts the numBits least-significant bits from a word. */
+    /** extracts the numBits least-significant bits from an unsigned word. */
     template <typename Word>
     static inline Word getLSB(Word word, int numBits);
 
@@ -24,35 +24,26 @@ namespace namedstruct {
         return numBits >= sizeof(Word) * 8 ? word : word & ((static_cast<Word>(1) << numBits) - 1);
     }
 
-#if __BMI__ && __x86_64
-#include <x86intrin.h>
+#if __x86_64
 #define NAMEDSTRUCT_USE_GET_LSB_NATIVE 1
 
     template <typename Word>
     inline static Word getLSB_native(Word word, int numBits) {
-        if constexpr (sizeof(Word) == 4) {
-            return _bextr_u32(word, 0, numBits);
-        }
-        else if constexpr (sizeof(Word) == 8) {
-            return _bextr_u64(word, 0, numBits);
-        }
-        else {
+        if constexpr (sizeof(Word) != 4 && sizeof(Word) != 8) {
             return getLSB_generic(word, numBits);
         }
-    }
-#endif
-
-#if __ARM_NEON
-#include <arm_neon.h>
-#define NAMEDSTRUCT_USE_GET_LSB_NATIVE 1
-
-    template <typename Word>
-    inline static Word getLSB_native(Word word, int numBits) {
-        if constexpr (sizeof(Word) == 4) {
-            return _arm_ubfx(word, 0, numBits);
-        }
         else {
-            return getLSB_generic(word, numBits);
+            constexpr auto adjustWidth = [](int n) {
+                return sizeof(Word) == 4 ? n : reinterpret_cast<Word&>(n);
+            }
+
+            Word result;
+            numBits <<= 8;
+            asm ("bextr %[numBits], %[word], %[result])"
+                 : [result] "=r" (result)
+                 : [word] "r" (word), [numBits] "r" (adjustWidth(numBits))
+                 : "cc");
+            return result;
         }
     }
 #endif
