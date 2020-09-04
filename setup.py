87cb8ee002d75181238c0ba41ab92365f1cd8a60
Cameron Monagle <cm@cameronmonagle.com>
2020-09-04 14:52:30 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+import os
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+            
+setup(name='pinsCompress',
+      version='0.1',
+      description='Compressor for sharing system map layers to the bpin format',
+      url='https://github.com/transitapp/namedstruct',
+      author='Transit',
+      license="Transit",
+      packages=['namedstruct'],
+      install_requires=required,
+      zip_safe=False
+)   
