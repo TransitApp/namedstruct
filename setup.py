ae0ccfe23848e7bf0e58ec6e5236ef1257d06195
Cameron Monagle <cm@cameronmonagle.com>
2020-09-04 14:55:59 -0400

    fixing name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 with open('requirements.txt') as f:
     required = f.read().splitlines()
             
-setup(name='pinsCompress',
+setup(name='namedstruct',
       version='0.1',
       description='Compressor for sharing system map layers to the bpin format',
       url='https://github.com/transitapp/namedstruct',
